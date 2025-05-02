d95ce432aea88d3aee095d30fd279bd772b3c319
RMSebastian <105923386+RMSebastian@users.noreply.github.com>
2025-05-02 16:08:36 -0300

    ADD: Constants

---
+++ src/constant/loggerColorList.ts
@@ -0,0 +1,6 @@
+export const loggerColorList = {
+  success: "text-green-500",
+  error: "text-red-500",
+  warning: "text-yellow-500",
+  info: "text-blue-500",
+};
---
+++ src/constant/navListItems.ts
@@ -0,0 +1,5 @@
+export const navItems = [
+  { name: "Kong", link: "/kong" },
+  { name: "Kafka", link: "/kafka" },
+  { name: "Health Checker", link: "/healthChecker" },
+];
