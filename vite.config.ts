94be4b15f03e418a9c5d35feb6cfb097917f2d6f
RMSebastian <105923386+RMSebastian@users.noreply.github.com>
2025-05-02 16:07:29 -0300

    ADD: configurations

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_APIQA_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+        "/obtener": {
+          target: env.VITE_MARKEY_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/obtener/, ""),
+        },
+      },
+    },
+  };
+});
